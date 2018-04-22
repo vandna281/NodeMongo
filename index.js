158ec5474b5c9ede89dc882fe4c2f66aed3171f3
Vandna <vandana.n@naukri.com>
2018-04-22 15:35:46 +0530

    Added different routes ...................

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
+const dishRouter = require('./dishRouter');
+const promoRouter = require('./promoRouter');
+const leadRouter = require('./leaderRouter');
+
 const express = require('express'),
 http = require('http');
 const morgan = require('morgan');
-const hostname = '192.168.123.16';
+const bodyParser = require('body-parser');
+
+const hostname = '192.168.114.3';
 const port = 3000;
+
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/'));
-app.use((req, res, next) => {
-  console.log(req.headers);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
-});
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leadRouter);
 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
